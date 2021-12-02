56c5aba59d3e49b996cc5b2cabf7b79c1c61c419
nabin <nkbhaie2017@gmail.com>
2021-12-02 16:20:26 -0700

    add form field types in wtforms form class

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask.scaffold import F
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, Pet
@@ -41,15 +41,49 @@ def show_all_pets():
     # pets = db.session.query.all()
     return render_template('home.html', pets = pets)
 
-@app.route('/add')
-def add_pet():
-    # Create a form for adding pets. This should use Flask-WTF, and should have the following fields:
-    # This should be at the URL path /add. Add a link to this from the homepage.
+# @app.route('/add')
+# def add_pet():
+#     # Create a form for adding pets. This should use Flask-WTF, and should have the following fields:
+#     # This should be at the URL path /add. Add a link to this from the homepage.
+#     form = AddPetForm()
+#     return render_template("add_pet.html", form = form)
+
+@app.route('/add', methods=["GET", "POST"])  # database does not render table, fix that and this works
+def handle_add_pet_form():
     form = AddPetForm()
-    return render_template("add_pet.html", form = form)
+    if form.validate_on_submit():
+        # get data from wtf. They are attributes in forms.py
+        nam = form.pet_name   
+        specy = form.pet_species  # now pet species has choices as a validator, use it here to work properly
+        url = form.pet_photo_url
+        ag = form.pet_age 
+        noot = form.pet_notes
+        # now pass these data to database, which is model class here
+        pet_obj = Pet(name = nam, species = specy,  photo_url = url, age = ag, notes = noot)
+        db.session.add(pet_obj)
+        db.session.commit()
+        # return redirect('/')
+        return redirect(url_for('show_all_pets')) # url_for takes method name as argument 
+    else:
+        return render_template("add_pet.html", form = form)
 
 
 """
+
+Step 5: Add Validation
+WTForms gives us lots of useful validators; we want to use these for validating our fields more carefully:
+
+the species should be either “cat”, “dog”, or “porcupine”
+the photo URL must be a URL (but it should still be able to be optional!)
+the age should be between 0 and 30, if provided
+
+Step 4: Create Handler for Add Pet Form
+This should validate the form:
+
+if it doesn’t validate, it should re-render the form
+if it does validate, it should create the new pet, and redirect to the homepage
+This should be a POST request to the URL path /add.
+
 Step 2: Make Homepage Listing Pets
 The homepage (at route /) should list the pets:
 
